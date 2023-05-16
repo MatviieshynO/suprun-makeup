86fd39f191d382efdebf3da44c00d7436253d872
MatviieshynO <Olopromotion@gmail.com>
2023-05-16 12:37:24 +0200

    dasd

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -18,7 +18,17 @@ const Content = () => {
           <img className="rounded-lg " src="images/content-image.png" alt="" />
         </div>
       </div>
-      <div className="flex-column sm:flex justify-around">
+      <div className="flex-column sm:hidden justify-around">
+        <div className="sm:pt-20">
+          <h4 className="text-[6vw] sm:text-[2.2vw] uppercase sm:py-4 text-center sm:pt-20 text-pink-700 pt-6">
+            Lorem ipsum dolor sit amet.
+          </h4>
+          <p className="text-[4vw] sm:text-[1.1vw] sm:max-w-[40vw] text-start max-w-[90vw] mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            labore quisquam suscipit, quibusdam ea id quo temporibus doloribus
+            expedita earum sequi qui necessitatibus laudantium pariatur.
+          </p>
+        </div>
         <div className="p-2 shadow-2xl shadow-black rounded-lg image-content-2">
           <img
             className="rounded-lg "
@@ -26,7 +36,7 @@ const Content = () => {
             alt=""
           />
         </div>
-        <div className="flex-column p-2">
+        <div className="hidden flex-column p-2">
           <div className="sm:pt-20">
             <h4 className="text-[6vw] sm:text-[2.2vw] uppercase sm:py-4 text-center sm:pt-20 text-pink-700 pt-6">
               Lorem ipsum dolor sit amet.
